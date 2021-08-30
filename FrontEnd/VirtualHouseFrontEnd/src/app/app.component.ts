baa5850a1178118509372ebce7359c883025d335
Isaac Porras <imanoisaac@gmail.com>
2021-08-30 14:14:28 -0600

    Creacion y validación de credenciales en login

--- FrontEnd/VirtualHouseFrontEnd/src/app/app.component.ts
+++ FrontEnd/VirtualHouseFrontEnd/src/app/app.component.ts
@@ -1,10 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import {ReactiveFormsModule,FormGroup, FormControl, Validators, FormBuilder} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+
+
+
+export class AppComponent implements OnInit{
+
   title = 'VirtualHouseFrontEnd';
+  username: string  = "";
+  password: string  = "";
+
+  
+
+
+  
+  onLogIn(username:string, password:string){
+    console.log(username);
+    console.log(password);
+    if(password == "admin" && username == "admin"){
+      console.log("Credenciales correctos");
+      alert("Credenciales correctos");
+    }
+    else{
+      console.log("Credenciales incorrectos");
+      alert("Credenciales incorrectos");
+    }
+  }
+
+
+  
+
+  ngOnInit() {
+
+  }
 }
