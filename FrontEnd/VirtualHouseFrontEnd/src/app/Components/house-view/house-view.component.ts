cf8e96ec06777b643045ef6553ef3fbb59d59e4f
Isaac Porras <imanoisaac@gmail.com>
2021-08-31 11:33:22 -0600

    Add canvas

--- FrontEnd/VirtualHouseFrontEnd/src/app/Components/house-view/house-view.component.ts
+++ FrontEnd/VirtualHouseFrontEnd/src/app/Components/house-view/house-view.component.ts
@@ -1,15 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import { Component,ViewChild, ElementRef, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-house-view',
   templateUrl: './house-view.component.html',
-  styleUrls: ['./house-view.component.css']
+  styleUrls: ['./house-view.component.css'],
+  template:`
+  <canvas #canvas width="600" height="300"></canvas>
+  <button (click)="animate()">Play</button>   
+`,
+styles: ['canvas { border-style: solid }']
 })
-export class HouseViewComponent implements OnInit {
 
-  constructor() { }
+
+
+export class HouseViewComponent implements OnInit {
+  @ViewChild('canvas', { static: true })
+  canvas: ElementRef<HTMLCanvasElement>;  
+  
+  private ctx: CanvasRenderingContext2D;
 
   ngOnInit(): void {
+    }
+
+  ngAfterViewInit() {
+    setTimeout(() => {
+      var canvasEl: HTMLCanvasElement = this.canvas.nativeElement;
+      var cx = canvasEl.getContext('2d');
+    
+    }, 1);
   }
+  
+  animate(): void {}
 
 }
